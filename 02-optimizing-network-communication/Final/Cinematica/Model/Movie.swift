3e6cc1b67bf2be0fef4acd3057ffff36c4d34bca
Mina Hanna <mina.hanna@kazyon.com>
2024-03-18 20:44:41 +0200

    Add Lesson 2

---
+++ 02-optimizing-network-communication/Final/Cinematica/Model/Movie.swift
@@ -0,0 +1,86 @@
+/// Copyright (c) 2024 Kodeco Inc.
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Foundation
+
+struct Movie: Codable, Identifiable, Equatable, Hashable {
+  let adult: Bool?
+  let backdropPath: String?
+  let genreIds: [Int]?
+  let id: Int?
+  let originalLanguage: String?
+  let originalTitle: String?
+  let overview: String?
+  let popularity: Double?
+  let posterPath: String?
+  let releaseDate: String?
+  let title: String?
+  let video: Bool?
+  let voteAverage: Double?
+  let voteCount: Int?
+
+  init(
+    adult: Bool?,
+    backdropPath: String?,
+    genreIds: [Int]?,
+    id: Int?,
+    originalLanguage: String?,
+    originalTitle: String?,
+    overview: String?,
+    popularity: Double?,
+    posterPath: String?,
+    releaseDate: String?,
+    title: String?,
+    video: Bool?,
+    voteAverage: Double?,
+    voteCount: Int?
+  ) {
+    self.adult = adult
+    self.backdropPath = backdropPath
+    self.genreIds = genreIds
+    self.id = id
+    self.originalLanguage = originalLanguage
+    self.originalTitle = originalTitle
+    self.overview = overview
+    self.popularity = popularity
+    self.posterPath = posterPath
+    self.releaseDate = releaseDate
+    self.title = title
+    self.video = video
+    self.voteAverage = voteAverage
+    self.voteCount = voteCount
+  }
+
+  var imageUrl: String? {
+    return AppConstants.imageBaseUrl + (posterPath ?? "")
+  }
+}
---
+++ 02-optimizing-network-communication/Final/Cinematica/View/MovieList/MovieListViewModel.swift
@@ -0,0 +1,72 @@
+/// Copyright (c) 2024 Kodeco Inc.
+/// 
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+/// 
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+/// 
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Foundation
+
+class MovieListViewModel: ObservableObject {
+  // MARK: - Properties
+  @Published var movies: [Movie] = []
+  @Published var isLoading = true
+  @Published var errorMessage: String?
+  private let requestManager = RequestManager()
+  private var currentPage = 1
+  private var totalPages = 1
+  private var isFetching = false
+
+  // MARK: - Methods
+  func fetchMovies() {
+    guard !isFetching && currentPage <= totalPages else { return }
+    isFetching = true
+
+    Task {
+      do {
+        let moviePaginatedResponse: MoviePaginatedResponse = try await
+          requestManager.perform(MoviesRequests.fetchNowPlaying(page: currentPage))
+        let newMovies = moviePaginatedResponse.results ?? []
+        await MainActor.run {
+          self.movies.append(contentsOf: newMovies)
+          self.totalPages = moviePaginatedResponse.totalPages ?? 1
+          self.isLoading = false
+          self.currentPage += 1
+          self.isFetching = false
+          self.errorMessage = nil
+        }
+      } catch {
+        await MainActor.run {
+          self.isLoading = false
+          self.errorMessage = error.localizedDescription
+          self.isFetching = false
+        }
+      }
+    }
+  }
+}
